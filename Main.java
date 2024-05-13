6e8a52899322104f17e7da1fe50dd2d475ad38b8
NotGreenParrot <167158456+NotGreenParrot@users.noreply.github.com>
2024-05-13 03:10:22 +0000

    saving the codespace

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World!");
+        /* student object feature test
+        Student waffle = new Student("pranahita", "prannynanny", "kapybara", 140504);
+        System.out.println(waffle); */
     }
 }
---
+++ Student.java
@@ -0,0 +1,27 @@
+public class Student {
+    private String firstName;
+    private String nickName;
+    private String lastName;
+    private int studentID;
+
+    public Student(String first, String nick, String last, int ID){
+        firstName = first;
+        nickName = nick;
+        lastName = last;
+        studentID = ID;
+    }
+
+    public Student(String first, String last, int ID){
+        firstName = first;
+        nickName = first;
+        lastName = last;
+        studentID = ID;
+    }
+
+    public String toString(){
+        if(firstName.equals(nickName)){
+            return firstName + " " + lastName + " ID: " + studentID;
+        }
+        return firstName + " \"" + nickName + "\" " + lastName + " ID: " + studentID;
+    }
+}
