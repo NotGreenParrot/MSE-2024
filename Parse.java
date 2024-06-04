008660b3c913b8c14cdf210a2b569326b9a8c8e6
NotGreenParrot <167158456+NotGreenParrot@users.noreply.github.com>
2024-06-04 12:47:06 +0000

    htmltest

--- Parse.java
+++ Parse.java
@@ -9,17 +9,15 @@ import java.util.ArrayList;
 
 
 public class Parse{
-    public static ArrayList<Student> list;
-    public static ArrayList<Teacher> list2;
     public static void main(String[] args){
-        list2 = new ArrayList<Teacher>();
         parseRec();
     }
     public static void parseRec() {
         try {  
             
-        list = new ArrayList<Student>();
-        list2 = new ArrayList<Teacher>();
+        ArrayList<Student> list = new ArrayList<Student>();
+        
+        ArrayList<Teacher> list2 = new ArrayList<Teacher>();
         File file = new File("/workspaces/MSE-2024/datastore/_2023 NHS Teacher Recommendation Form (Responses for Project) .xlsx");   //creating a new file instance  
         FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file  
         //creating Workbook instance that refers to .xlsx file  
@@ -127,6 +125,11 @@ public class Parse{
         break;  
         default:  
          }  */
+
+         for(int i = 0; i < list2.size(); i++){
+            list2.get(i).print();
+            System.out.println();
+        }
         }  
         System.out.println("");  
         }  
@@ -135,6 +138,95 @@ public class Parse{
         {  
         e.printStackTrace();  
         }  
+    }
+
+    public static void ParseReq() {
+        
+        try {  
+            ArrayList<Student> list = new ArrayList<Student>();
+            
+            ArrayList<Teacher> list2 = new ArrayList<Teacher>();
+            File file = new File("/workspaces/MSE-2024/datastore/NHS Teacher Rec Requests .xlsx");   //creating a new file instance  
+            FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file  
+            //creating Workbook instance that refers to .xlsx file  
+            XSSFWorkbook wb = new XSSFWorkbook(fis);   
+            XSSFSheet sheet = wb.getSheetAt(0);     //creating a Sheet object to retrieve object  
+            Iterator<Row> itr = sheet.iterator();    //iterating over excel file  
+            for(int i = 0; i < 1; i++){
+                itr.next();
+            } // set up the Apache POI
+    
+            while (itr.hasNext()) {  
+            Row row = itr.next();  
+            Iterator<Cell> cellIterator = row.cellIterator();   //iterating over each column 
+            cellIterator.next(); //set up and iterate through rows
+             
+            Cell cell = cellIterator.next(); 
+            if(!checkIfRowIsEmpty(row) /* || row.getCellType() == Cell.CELL_TYPE_BLANK*/) {
+            
+            cell = cellIterator.next();
+            String last;
+            if(cell.getCellType() == Cell.CELL_TYPE_STRING) {
+                last = cell.getStringCellValue();
+            } else {
+                last = "";
+            }
+
+            cell = cellIterator.next();
+            String first;
+            if(cell.getCellType() == Cell.CELL_TYPE_STRING) {
+                first = cell.getStringCellValue();
+            } else {
+                first = "";
+            }
+
+                
+            cell = cellIterator.next();
+            int id;
+            if(cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+                id = (int)cell.getNumericCellValue();
+            } else {
+                id  = 0;
+            }
+    
+            cell = cellIterator.next();
+           
+    
+    
+    
+            int loc = -1; // location of teacher in the teacher arraylist
+            for(int i = 0; i < list2.size(); i++){
+               Teacher t = list2.get(i);
+                if(teacher.equals(t.getLast())){
+                    loc = i;
+                }
+            } 
+            
+            if(loc == -1){ // creates new teacher object if teacher not found, otherwise adds the student to existing teacher
+                Teacher teach = new Teacher(teacher, email);
+                list2.add(teach);
+                teach.add(add);
+            } else {
+                list2.get(loc).add(add);
+            }
+    
+            /*switch (cell.getCellType()){  
+            case Cell.CELL_TYPE_STRING:    //field that represents string cell type  
+            System.out.print(cell.getStringCellValue() + "\t\t\t");  
+            break;  
+            case Cell.CELL_TYPE_NUMERIC:    //field that represents number cell type  
+            System.out.print(cell.getNumericCellValue() + "\t\t\t");  
+            break;  
+            default:  
+             }  */
+            }  
+            System.out.println("");  
+            }  
+            }  
+            catch(Exception e)  
+            {  
+            e.printStackTrace();  
+            }  
     }
 
     private static boolean checkIfRowIsEmpty(Row row) { // checks if row is empty
