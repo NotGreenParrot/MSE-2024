3c8ad4ebdf12b7d9ddc7bc2c1da744c486873550
NotGreenParrot <167158456+NotGreenParrot@users.noreply.github.com>
2024-06-05 04:15:54 +0000

    .

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
+import java.io.IOException;
+import java.util.Properties;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args ) throws IOException{
         System.out.println("Hello World!");
         //Parse.parseRec();
         Parse.parseReq();
-        
+        Properties properties = new Properties();
+/*to access your filename.properties file */
+        properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("filename.properties"));
+        String  sServerLocation = properties.getProperty("server.upload.docs.path");
+        System.out.println(sServerLocation);
     }
 }
